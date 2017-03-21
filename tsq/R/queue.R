fab7427f1d7328ee5f0754824510ba0367810c2c
Eric Tan <ejtan@ucdavis.edu>
2017-03-21 14:35:01 -0700

    Added tsq and made stack man pages

--- tsq/R/queue.R
+++ tsq/R/queue.R
@@ -1,44 +1,46 @@
 # ECS 145 Project
 # Queue Implamentation
 
-queue <- function() {
+# Declare generic functions
+pop <- function(inqueue) UseMethod('pop')
+push <- function(inqueue, item) UseMethod('push')
+
+
+newqueue <- function() {
     rtrn <- list()
-    class(rtrn) <- "queue"
-    
+    class(rtrn) <- 'queue'
+
     # Head and tail tracks position of the list
     rtrn$head <- 1
     rtrn$tail <- 1
-    
     # Length tracks the size of the queue
     rtrn$length <- 0
-    
+
     # data is a list which stores the queue data.
     # Initalized te first element to NA
     rtrn$data <- vector()
     rtrn$data[1] <- NA
-    
+
     return(rtrn);
 }
 
 
 print.queue <- function(inqueue) {
-    if (inqueue$head == inqueue$tail)
-        # if head and tail are the same, there is 1 no element
-        queue_idx = inqueue$head
-    else
+    if (inqueue$head != inqueue$tail) {
         # generate range of data in the queue
         queue_idx = inqueue$head:(inqueue$tail-1)
-    print(inqueue$data[queue_idx])
+        print(inqueue$data[queue_idx])
+    }
 }
 
 
 push.queue <- function(inqueue, item) {
-    inqueue$data[inqueue$tail] <- item
-    
-    inqueue$tail <- inqueue$tail + 1
-    inqueue$length <- inqueue$length + 1
-    
-    return(inqueue)
+    # Insert item
+    eval.parent(substitute(inqueue$data[inqueue$tail] <- item))
+
+    # Update length and tail
+    eval.parent(substitute(inqueue$tail <- inqueue$tail + 1))
+    eval.parent(substitute(inqueue$length <- inqueue$length + 1))
 }
 
 
@@ -49,8 +51,13 @@ pop.queue <- function(inqueue) {
         stop()
     }
     else {
+        # take out element at the head
         element <- inqueue$data[inqueue$head]
+
+        # Update popped element to NA
         eval.parent(substitute(inqueue$data[inqueue$head] <- NA))
+
+        # Update length and head
         eval.parent(substitute(inqueue$head <- inqueue$head + 1))
         eval.parent(substitute(inqueue$length <- inqueue$length - 1))
         return(element)
--- tsq/R/stack.R
+++ tsq/R/stack.R
@@ -1,56 +1,64 @@
 # ECS 145 Project
 # Stack Implamentation
 
-stack <- function() {
+pop <- function(instack) UseMethod('pop')
+push <- function(instack, item) UseMethod('push')
+
+newstack <- function() {
     rtrn <- list()
     class(rtrn) <- "stack"
-    
-    # Head and tail tracks position of the list
-    rtrn$head <- 1
-    rtrn$tail <- 1
-    
+
+    # top tracks top of the stack
+    rtrn$top <- 1
+
     # Length tracks the size of the stack
     rtrn$length <- 0
-    
+
     # data is a list which stores the stack data.
     # Initalized te first element to NA
     rtrn$data <- vector()
     rtrn$data[1] <- NA
-    
+
     return(rtrn);
 }
 
 
-print.stack <- function(inqueue) {
-    if (inqueue$head == inqueue$tail)
-        # if head and tail are the same, there is 1 element
-        stack_idx = inqueue$head
-    else
-        # generate range of data in the queue
-        stack_idx = (inqueue$head-1):(inqueue$tail)
-
-    if(inqueue$head == 1 && inqueue$tail == 1)
-      print(vector())
-    else
-      print(inqueue$data[stack_idx])
+print.stack <- function(instack) {
+    if (instack$length != 0) {
+        # generate range of data in the stack
+        stack_idx = 1:(instack$top - 1)
+        print(instack$data[stack_idx])
+    }
 }
 
 
-push.stack <- function(inqueue, item) {
-    inqueue$data[inqueue$head] <- item
-    
-    inqueue$head <- inqueue$head + 1
-    inqueue$length <- inqueue$length + 1
-    
-    return(inqueue)
+push.stack <- function(instack, item) {
+    # Insert item
+    eval.parent(substitute(instack$data[instack$top] <- item))
+
+    # Update length and top
+    eval.parent(substitute(instack$top <- instack$top + 1))
+    eval.parent(substitute(instack$length <- instack$length + 1))
 }
 
 
-pop.stack <- function(inqueue) {
-    element <- inqueue$data[inqueue$head]
-    
-    eval.parent(substitute(inqueue$head <- inqueue$head - 1))
-    eval.parent(substitute(inqueue$length <- inqueue$length - 1))
-    
-    return(element)
+pop.stack <- function(instack) {
+    if (instack$length == 0) {
+        # Check if stack is empty
+        warning("Popping empty stack!")
+        stop()
+    }
+    else {
+        # Take element at the top
+        element <- instack$data[instack$top - 1]
+
+        # Set popped element to NA
+        eval.parent(substitute(instack$data[instack$top - 1] <- NA))
+
+        # Update top and length
+        eval.parent(substitute(instack$top<- instack$top - 1))
+        eval.parent(substitute(instack$length <- instack$length - 1))
+
+        return(element)
+    }
 }
